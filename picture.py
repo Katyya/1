24c52c91e7dadc2863534e044dc84d7b90b66948
Katyya <katrin-st@inbox.ru>
2020-02-03 20:09:37 +0400

    second

--- picture.py
+++ picture.py
@@ -38,27 +38,6 @@ def clouds(x,y,r):
     circle(x + (2*r), y, r)
     circle(x + ((7 * r)/3), y+r, r)
 
-def bord():
-    brushColor("Brown")
-    penColor(0, 0, 0)
-    circle(250,450,50)
-    rectangle(250, 450, 450, 500)
-    brushColor(0, 0, 255)
-    penColor(0, 0, 255)
-    rectangle(200, 400, 300, 450)
-    brushColor("Brown")
-    polygon ( [(450,450), (540,450), (450,500)] )
-    penColor(0, 0, 0)
-    brushColor(255,255,255)
-    penSize(5)
-    circle(480, 465, 10)
-    brushColor(0,0,0)
-    rectangle(350, 200, 365, 450)
-    penSize(1)
-    brushColor(153, 133, 120)
-    polygon([(365, 200), (450, 325), (390, 325)])
-    polygon([(365, 450), (450, 325), (390, 325)])
-
 def sun_unbrella():
     brushColor("Brown")
     penColor(0, 0, 0)
@@ -68,10 +47,48 @@ def sun_unbrella():
         polygon([(125, 580), (30+h, 650), (70+h, 650)])
         h+=40
 
+
+brushColor("Brown")
+penColor(0, 0, 0)
+a= circle(250,450,50)
+"""задняя часть корабля"""
+b=rectangle(250, 450, 450, 500)
+"""средняя часть корабля"""
+с=polygon ( [(450,450), (540,450), (450,500)] )
+"""передняя часть"""
+brushColor(0, 0, 255)
+penColor(0, 0, 255)
+rectangle(200, 400, 1000, 450)
+"""вода"""
+penColor(0, 0, 0)
+brushColor(255,255,255)
+penSize(5)
+w=circle(480, 465, 10)
+"""windows"""
+brushColor(0,0,0)
+m=rectangle(350, 200, 365, 450)
+"""Матча"""
+penSize(1)
+brushColor(153, 133, 120)
+p1=polygon([(365, 200), (450, 325), (390, 325)])
+p2=polygon([(365, 450), (450, 325), (390, 325)])
+"""парус"""
+board=(a,b)
+def update():
+    if 700 >= xCoord(a):
+        moveObjectBy(a, 5, 0)
+        moveObjectBy(b, 5, 0)
+        moveObjectBy(с, 5, 0)
+        moveObjectBy(w, 5, 0)
+        moveObjectBy(m, 5, 0)
+        moveObjectBy(p1, 5, 0)
+        moveObjectBy(p2, 5, 0)
+
+onTimer(update, 50)
 sun()
 clouds(220,150,30)
 clouds(470,100,40)
-bord()
+
 sun_unbrella()
 
 run()
